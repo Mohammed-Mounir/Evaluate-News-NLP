10708398995a4b4553694d4775c1f36785c33e45
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-25 20:09:43 +0200

    fix: functionality

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,10 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
   mode: "development",
   devtool: "source-map",
   stats: "verbose",
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,10 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
   mode: "production",
   module: {
     rules: [
