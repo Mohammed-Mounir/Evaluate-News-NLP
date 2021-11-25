bbd0b3126957dd3b8f33179450502f0dddd3674d
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-25 18:11:14 +0200

    WIP

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,25 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require("dotenv");
+const path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
 
-const app = express()
+dotenv.config({ path: `${__dirname}/./../../.env` });
+const app = express();
 
-app.use(express.static('dist'))
+console.log(`Your API key is ${process.env.API_KEY}`);
 
-console.log(__dirname)
+app.use(express.static("dist"));
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+  // res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+  console.log("Example app listening on port 8080!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
