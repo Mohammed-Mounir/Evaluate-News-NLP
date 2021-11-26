7222d734cd66b5128f076c31faeb2a5b83eb7160
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-26 21:38:37 +0200

    refactor: test

--- src/client/__test__/checkValidUrl.test.js
+++ src/client/__test__/checkValidUrl.test.js
@@ -1,6 +1,10 @@
 import { checkValidUrl } from "../js/checkValidUrl";
 
 describe("Test checkValidUrl functionality", () => {
+  test("Check if checkValidUrl function is defined", () => {
+    expect(checkValidUrl()).toBeDefined();
+  });
+
   test("Check if a valid http url returns true", () => {
     expect(checkValidUrl("https://www.google.com/")).toBeTruthy();
   });
---
+++ src/client/__test__/formHandler.test.js
@@ -0,0 +1,7 @@
+import { handleSubmit } from "../js/formHandler";
+
+describe("Testing the submit functionality", () => {
+  test("Testing the handleSubmit() function", () => {
+    expect(handleSubmit).toBeDefined();
+  });
+});
